dc7e3c1eb848fab7508161904f7f53f38083bb16
OMNICOM\Tiju.RajanAbraham <tiju.rajanabraham@au.tribalworldwide.com>
2019-04-15 10:55:04 +1000

    Initial Commit

---
+++ CenturyAirline/Controllers/AuthController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using System.Web.Security;
+using Umbraco.Web.Mvc;
+using CenturyAirline.Models;
+
+namespace CenturyAirline.Controllers
+{
+    public class AuthController : SurfaceController
+    {
+        public ActionResult RenderLogin()
+        {
+            return PartialView("_Login", new LoginModel());
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult SubmitLogin(LoginModel model, string returnUrl)
+        {
+            if (ModelState.IsValid)
+            {
+                if (Membership.ValidateUser(model.Username, model.Password))
+                {
+                    FormsAuthentication.SetAuthCookie(model.Username, false);
+                    UrlHelper myHelper = new UrlHelper(HttpContext.Request.RequestContext);
+                    if (myHelper.IsLocalUrl(returnUrl))
+                    {
+                        return Redirect(returnUrl);
+                    }
+                    else
+                    {
+                        return Redirect("/");
+                    }
+                }
+                else
+                {
+                    ModelState.AddModelError("", "The username or password provided is incorrect.");
+                }
+            }
+            return CurrentUmbracoPage();
+        }
+
+        public ActionResult RenderLogout()
+        {
+            return PartialView("_Logout", null);
+        }
+
+        public ActionResult SubmitLogout()
+        {
+            TempData.Clear();
+            Session.Clear();
+            FormsAuthentication.SignOut();
+            return RedirectToCurrentUmbracoPage();
+        }
+    }
+}
---
+++ CenturyAirline/Models/LoginModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace CenturyAirline.Models
+{
+    public class LoginModel
+    {
+        [Display(Name = "Username")]
+        [Required]
+        public string Username { get; set; }
+
+        [Display(Name = "Password")]
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
